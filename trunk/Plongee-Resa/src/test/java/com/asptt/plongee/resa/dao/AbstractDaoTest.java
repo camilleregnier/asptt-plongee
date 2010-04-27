1599630de6363ae93ef82b4fc4f26163c0ce9d3f
nokyyz@gmail.com <nokyyz@gmail.com@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-04-27 21:30:17 +0000

    Renommage des fichiers spring (ajout du type d'implémentation dans le nom du fichier)

--- trunk/Plongee-Resa/src/test/java/com/asptt/plongee/resa/dao/AbstractDaoTest.java
+++ trunk/Plongee-Resa/src/test/java/com/asptt/plongee/resa/dao/AbstractDaoTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/spring/spring-dao-test.xml", "classpath:/spring/spring-datasource-test.xml"})
+@ContextConfiguration(locations={"classpath:/spring/spring-dao-jdbc.xml", "classpath:/spring/spring-datasource-test.xml"})
 public abstract class AbstractDaoTest {
 
 
