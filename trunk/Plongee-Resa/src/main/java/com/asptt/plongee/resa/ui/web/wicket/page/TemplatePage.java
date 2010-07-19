ac0fddb8738eeaaf734d3eb82e03627cd2fc9d91
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-07-19 17:34:44 +0000

    commit camille

--- trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/TemplatePage.java
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/TemplatePage.java
@@ -1,10 +1,34 @@
 package com.asptt.plongee.resa.ui.web.wicket.page;
 
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 
 import com.asptt.plongee.resa.ui.web.wicket.ResaSession;
 
 public abstract class TemplatePage extends WebPage {
+	MarkupContainer userMenus = new MarkupContainer("menuUser"){  
+		  
+		  @Override  
+		  public boolean isVisible() {     
+		   return getResaSession().get().getRoles().hasRole("USER");  
+		  }  
+		   
+	}; 
+	
+	MarkupContainer secretariatMenus = new MarkupContainer("menuSecretariat"){  
+		  
+		  @Override  
+		  public boolean isVisible() {     
+		   return getResaSession().get().getRoles().hasRole("SECRETARIAT");  
+		  }  
+		   
+	};
+	
+	public TemplatePage() {
+		super();
+		 add(userMenus);
+		 add(secretariatMenus);
+	}
 
 	public ResaSession getResaSession(){
 		return (ResaSession) getSession();
