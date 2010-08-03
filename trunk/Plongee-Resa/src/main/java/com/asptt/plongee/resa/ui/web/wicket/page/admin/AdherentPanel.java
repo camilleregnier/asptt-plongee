d6f19ee1949fd0bbecee53f885fd689f6d71600c
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-08-03 21:07:20 +0000

    Mise à jour pour mise en commun

--- trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/AdherentPanel.java
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/AdherentPanel.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.behavior.BehaviorsUtil;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
@@ -20,6 +23,9 @@ import com.asptt.plongee.resa.ui.web.wicket.ResaSession;
 import com.asptt.plongee.resa.ui.web.wicket.page.AccueilPage;
 
 public class AdherentPanel extends Panel {
+	
+	private CompoundPropertyModel model;
+	
 	public AdherentPanel(String id, IModel<Adherent> adherent) {
 		super(id, adherent);
 		setOutputMarkupId(true);
@@ -33,9 +39,9 @@ public class AdherentPanel extends Panel {
 		private static final long serialVersionUID = 5374674730458593314L;
 
 		public AdherentForm(String id, IModel<Adherent> adherent) {
-			super(id);
+			super(id,adherent);
 			
-			CompoundPropertyModel model = new CompoundPropertyModel(adherent);
+			model = new CompoundPropertyModel(adherent);
 			setModel(model);
 
 			add(new RequiredTextField<String>("nom"));
@@ -53,19 +59,33 @@ public class AdherentPanel extends Panel {
 				niveaux.add(n.toString());
 			}
 			add(new DropDownChoice("niveau", niveaux));
+			
+			// Ajout de la checkbox pilote
+			add(new CheckBox("pilote", model.bind("pilote")));
+
+			// Ajout de la checkbox directeur de plongée
+			final CheckBox checkDp = new CheckBox("dp", model.bind("dp"));
+			checkDp.setOutputMarkupId(true);
+			add(checkDp);
 
 			// Ajout de la liste des niveaux d'encadrement
 			List<String> encadrement = new ArrayList<String>();
 			for (Adherent.Encadrement e : Adherent.Encadrement.values()) {
 				encadrement.add(e.toString());
 			}
-			add(new DropDownChoice("encadrement", encadrement));
-
-			// Ajout de la checkbox pilote
-			add(new CheckBox("pilote", model.bind("pilote")));
-
-			// Ajout de la checkbox directeur de plongée
-			add(new CheckBox("dp", model.bind("dp")));
+			DropDownChoice encadrt = new DropDownChoice("encadrement", encadrement);
+			encadrt.add (new AjaxFormComponentUpdatingBehavior("onchange"){
+		            protected void onUpdate(AjaxRequestTarget target) {
+		            	CheckBox check = (CheckBox)get("dp");
+		            	Adherent a = (Adherent) model.getChainedModel().getObject();
+		                if (a.getEncadrement().contains("E"))
+		                	a.setDp(true);
+		                model.setObject(a);
+		                checkDp.setModel(model.bind("dp"));
+		                target.addComponent(checkDp);
+		            }
+				});
+			add(encadrt);
 
 			// Ajout des roles
 			List<String> roles = Arrays.asList(new String[] { "ADMIN", "USER",
@@ -81,7 +101,8 @@ public class AdherentPanel extends Panel {
 			Adherent adherent = (Adherent) getModelObject();
 
 			// Mise à jour de l'adhérent
-			ResaSession resaSession = (ResaSession) getApplication().getSessionStore().lookup(getRequest());
+			ResaSession resaSession = (ResaSession) getApplication()
+					.getSessionStore().lookup(getRequest());
 			resaSession.getAdherentService().updateAdherent(adherent);
 
 			setResponsePage(AccueilPage.class);
