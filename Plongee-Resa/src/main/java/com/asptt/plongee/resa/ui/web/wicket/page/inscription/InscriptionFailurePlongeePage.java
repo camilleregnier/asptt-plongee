1246aebd872e290ff714db71a4a1703a3e3df95c
eric.simon28@orange.fr <eric.simon28@orange.fr@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-08-03 17:38:12 +0000

    .

---
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/inscription/InscriptionFailurePlongeePage.java
@@ -0,0 +1,23 @@
+package com.asptt.plongee.resa.ui.web.wicket.page.inscription;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+
+import com.asptt.plongee.resa.model.Adherent;
+import com.asptt.plongee.resa.model.InscritsPlongeeDataProvider;
+import com.asptt.plongee.resa.model.Plongee;
+import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
+
+public class InscriptionFailurePlongeePage extends TemplatePage {
+
+	@SuppressWarnings("serial")
+	public InscriptionFailurePlongeePage(Plongee plongee) {
+		
+		Adherent a = getResaSession().getAdherent();
+	    add(new Label("hello","DESOLE, "+a.getPrenom()+" Inscription impossible sur cette plongée"));
+	}
+
+}
