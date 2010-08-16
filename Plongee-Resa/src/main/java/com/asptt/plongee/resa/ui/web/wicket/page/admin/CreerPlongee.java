8f2b610da7a5ae6d4f8fe22192f7c2a004165136
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-08-16 20:25:20 +0000

    Pages secretariat et modifications diverses sur le fenetres modales (plus de message intempestif lors de la fermeture de ce type de fenêtres)

---
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/CreerPlongee.java
@@ -0,0 +1,75 @@
+package com.asptt.plongee.resa.ui.web.wicket.page.admin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.wicket.datetime.StyleDateConverter;
+import org.apache.wicket.datetime.markup.html.form.DateTextField;
+import org.apache.wicket.extensions.yui.calendar.DatePicker;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.PropertyModel;
+
+import com.asptt.plongee.resa.model.NiveauAutonomie;
+import com.asptt.plongee.resa.model.Plongee;
+import com.asptt.plongee.resa.ui.web.wicket.page.AccueilPage;
+import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
+
+public class CreerPlongee extends TemplatePage {
+	
+	private static final Locale LOCALE_EN = new Locale("en");
+	
+	public CreerPlongee() {
+		// Constructeur du formulaire et du feedback panel pour renvoyer des messages sur la page
+		final FeedbackPanel feedback = new FeedbackPanel("feedback");
+		add(feedback);
+		add(new PlongeeForm("inputForm"));
+	}
+
+	public class PlongeeForm extends Form<Plongee> {
+
+		private static final long serialVersionUID = 5374674730458593314L;
+
+		public PlongeeForm(String id) {
+			super(id);
+			CompoundPropertyModel<Plongee> model = new CompoundPropertyModel<Plongee>(new Plongee());
+			setModel(model);
+			
+			DateTextField dateTextFiled = new DateTextField("date", new PropertyModel<Date>(model, "date"), new StyleDateConverter("S-", true));
+			add(dateTextFiled);
+			dateTextFiled.add(new DatePicker());
+			
+			add(new RequiredTextField<Integer>("nbMaxPlaces", Integer.class));
+			
+			
+			// Ajout de la liste des niveaux
+			List<String> niveaux = new ArrayList<String>();
+			for (NiveauAutonomie n : NiveauAutonomie.values()){
+				niveaux.add(n.toString());
+			}
+			add(new DropDownChoice("niveauMinimum", niveaux));
+			
+			//Ajout du type de plongee
+			List<String> type = Arrays.asList(new String[] { "MATIN", "APRES_MIDI", "SOIR", "NUIT" });
+			add(new DropDownChoice<String>("type", type));
+			
+		}
+
+		public void onSubmit() {
+			Plongee plongee = (Plongee) getModelObject();
+			
+			getResaSession().getPlongeeService().creerPlongee(plongee);
+
+			setResponsePage(AccueilPage.class);
+		}
+
+	}
+
+
+}
---
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/inscription/secretariat/InscriptionExterieurPlongeePage.java
@@ -0,0 +1,127 @@
+package com.asptt.plongee.resa.ui.web.wicket.page.inscription.secretariat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.string.Strings;
+import org.wicketstuff.objectautocomplete.AutoCompletionChoicesProvider;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteBuilder;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteField;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteRenderer;
+
+import com.asptt.plongee.resa.model.Adherent;
+import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.InscriptionPlongeePage;
+
+public class InscriptionExterieurPlongeePage extends TemplatePage {
+	
+	private ModalWindow modalExterieur;
+
+	public InscriptionExterieurPlongeePage() {
+		super();
+		add(new ExterieurForm("form"));
+		
+
+		// Lien pour la création du plongeur extérieur
+		modalExterieur = new ModalWindow("modalExterieur");
+		modalExterieur.setTitle("This is modal window with panel content.");
+		modalExterieur.setCookieName("modal-exterieur");
+		add(modalExterieur);
+		
+		add(new AjaxLink("creerExterieur") {
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				replaceModalWindow(target);
+				modalExterieur.show(target);
+
+			}
+		});
+
+	}
+
+	public List<Adherent> getMatchingAdherents(String search) {
+		if (Strings.isEmpty(search)) {
+			List<Adherent> emptyList = Collections.emptyList();
+			return emptyList;
+		}
+		
+		List<Adherent> list = getResaSession().getAdherentService()
+				.rechercherExternes();
+		
+		ArrayList<Adherent> newList = new ArrayList<Adherent>();
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getNom().startsWith(search.toUpperCase())) {
+				newList.add(list.get(i));
+			}
+		}
+		return newList;
+	}
+	
+	class ExterieurForm extends Form{
+		
+		ObjectAutoCompleteField<Adherent, String> autocompleteField ;
+
+		public ExterieurForm(String id) {
+			super(id);
+			
+			
+			AutoCompletionChoicesProvider<Adherent> provider = new AutoCompletionChoicesProvider<Adherent>() {
+				private static final long serialVersionUID = 1L;
+
+				public Iterator<Adherent> getChoices(String input) {
+					return getMatchingAdherents(input).iterator();
+				}
+			};
+			
+			ObjectAutoCompleteRenderer<Adherent> renderer = new ObjectAutoCompleteRenderer<Adherent>(){
+				private static final long serialVersionUID = 1L;
+
+				protected String getIdValue(Adherent adherent) {
+					return adherent.getNumeroLicense();
+				}
+				protected String getTextValue(Adherent adherent) {
+					String texteAffiche = adherent.getNom() + " " + adherent.getPrenom() + " " + adherent.getNiveau();
+					return texteAffiche;
+				}
+			};
+			
+			ObjectAutoCompleteBuilder<Adherent, String> builder = new ObjectAutoCompleteBuilder<Adherent, String>(provider);
+			builder.autoCompleteRenderer(renderer);
+			builder.searchLinkText("Autre recherche");
+			builder.width(200);
+
+
+			autocompleteField = builder.build("numeroLicense", new Model<String>());
+			final TextField<String> adherent = autocompleteField.getSearchTextField();
+			adherent.setRequired(true);
+			
+			add(autocompleteField);
+		}
+		
+		public void onSubmit() {
+			setResponsePage(new InscriptionPlongeePage(getResaSession().getAdherentService().rechercherAdherentParIdentifiant(autocompleteField.getConvertedInput())));
+		}
+		
+	}
+	
+	private void replaceModalWindow(AjaxRequestTarget target) {
+		modalExterieur.setContent(new ExterieurPanel(modalExterieur.getContentId()));
+		modalExterieur.setTitle("Compl&eacute;tez les  informations concernant le plongeur ext&eacute;rieur");
+		
+		// La hauteur de la fenetre s'adapte à son contenu
+		modalExterieur.setUseInitialHeight(false);
+		
+		// Pour éviter le message de disparition de la fenetre lors de la validation
+		target.appendJavascript( "Wicket.Window.unloadConfirmation  = false;");
+	}
+
+}
