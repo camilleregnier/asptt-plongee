a53080d311b30c4036d97013d15c85090218a3d2
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-18 16:10:37 +0000

    Ajout de la contrainte de taille sur le mot de passe

--- Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/ModifPasswordPage.java
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/ModifPasswordPage.java
@@ -7,6 +7,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.validation.validator.StringValidator.ExactLengthValidator;
 
 import com.asptt.plongee.resa.ui.web.wicket.ResaSession;
 
@@ -16,48 +17,68 @@ public class ModifPasswordPage extends WebPage {
 		super();
 		add(new ModifPasswordForm("modifPasswordForm"));
 	}
-	
-	private class ModifPasswordForm extends Form{
+
+	private class ModifPasswordForm extends Form {
 
 		public ModifPasswordForm(String id) {
 			super(id);
-			
+
 			final FeedbackPanel feedback = new FeedbackPanel("feedback");
 			feedback.setOutputMarkupId(true);
 			add(feedback);
+
+			final PasswordTextField oldPassword = new PasswordTextField(
+					"oldPassword", new Model<String>());
+			final PasswordTextField newPassword1 = new PasswordTextField(
+					"newPassword1", new Model<String>());
+			final PasswordTextField newPassword2 = new PasswordTextField(
+					"newPassword2", new Model<String>());
 			
-			final PasswordTextField oldPassword = new PasswordTextField("oldPassword", new Model<String>());
-			final PasswordTextField newPassword1 = new PasswordTextField("newPassword1", new Model<String>());
-			final PasswordTextField newPassword2 = new PasswordTextField("newPassword2", new Model<String>());
+			// Contrainte sur le password : entre 6 et 8 caractères
+			oldPassword.add(ExactLengthValidator.lengthBetween(6, 8));
+			newPassword1.add(ExactLengthValidator.lengthBetween(6, 8));
+			newPassword2.add(ExactLengthValidator.lengthBetween(6, 8));
 			
 			add(oldPassword.setRequired(true));
 			add(newPassword1.setRequired(true));
 			add(newPassword2.setRequired(true));
-			
+
 			add(new AjaxButton("validPlongee") {
-				
+
 				@Override
 				protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-					// Si les 2 nvx password sont égaux
-					if (newPassword1.getValue().equals(newPassword2.getValue())){
-						// Et qu'il sont différent de l'ancien mot de passe
-						if (!newPassword1.getValue().equals(oldPassword.getValue())){
-							// Mise à jour du mot de passe
-							((ResaSession) getSession()).getAdherent().setPassword(newPassword1.getValue());
-							
-							// Redirection vers l'accueil
-							setResponsePage(AccueilPage.class);
-						}else{
+
+					// L'ancien mot de passe saisi doit bien correspondre à l'ancien
+					if (oldPassword.getValue().equalsIgnoreCase(
+							((ResaSession) getSession()).getAdherent()
+									.getPassword())) {
+						// Si les 2 nvx password sont égaux
+						if (newPassword1.getValue().equals(
+								newPassword2.getValue())) {
+							// Et qu'il sont différent de l'ancien mot de passe
+							if (!newPassword1.getValue().equals(
+									oldPassword.getValue())) {
+								// Mise à jour du mot de passe
+								((ResaSession) getSession()).getAdherent()
+										.setPassword(newPassword1.getValue());
+
+								// Redirection vers l'accueil
+								setResponsePage(AccueilPage.class);
+							} else {
+								// Re-saisie du nouveau mot de passe
+								error("Le nouveau mot de passe doit être différent de celui d'origine");
+							}
+						} else {
 							// Re-saisie du nouveau mot de passe
-							error("Le nouveau mot de passe doit être différent de celui d'origine");
+							error("Erreur dans la saisie du nouveau mot de passe");
 						}
-					}else{
+					} else {
 						// Re-saisie du nouveau mot de passe
-						error("Erreur dans la saisie du nouveau mot de passe");
+						error("L'ancien mot de passe n'est pas correcte");
 					}
-					
+
 				}
-				
+
 				// L'implémentation de cette méthode est nécessaire pour voir
 				// les messages d'erreur dans le feedBackPanel
 				protected void onError(AjaxRequestTarget target, Form<?> form) {
@@ -65,8 +86,7 @@ public class ModifPasswordPage extends WebPage {
 				}
 			});
 		}
-		
+
 	}
-	
 
 }
