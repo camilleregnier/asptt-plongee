582223cbf437a9e4574dbdf611d50066d64a67b0
eric.simon28@orange.fr <eric.simon28@orange.fr@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-20 20:09:41 +0000

    ajax bouton dans le two

--- trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/GererListeAttenteTwo.java
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/GererListeAttenteTwo.java
@@ -4,22 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.extensions.markup.html.form.palette.Palette;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.util.CollectionModel;
 import org.apache.wicket.model.util.ListModel;
 
+import com.asptt.plongee.resa.exception.TechnicalException;
 import com.asptt.plongee.resa.model.Adherent;
 import com.asptt.plongee.resa.model.Plongee;
+import com.asptt.plongee.resa.ui.web.wicket.page.AccueilPage;
 import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
 
 public class GererListeAttenteTwo extends TemplatePage {
+	
+	private FeedbackPanel feedback = new FeedbackPanel("feedback");
+
 	public GererListeAttenteTwo(final Plongee plongee)
 	{
+		feedback.setOutputMarkupId(true);
+		add(feedback);
+
 		List<Adherent> persons = getResaSession().getPlongeeService().rechercherListeAttente(plongee);
 		IChoiceRenderer<Adherent> renderer = new ChoiceRenderer<Adherent>("nomComplet", "nom");
 		
@@ -39,22 +50,53 @@ public class GererListeAttenteTwo extends TemplatePage {
 
 		Form<?> form = new Form("form")
 		{
+
+			
+//			@Override
+//			protected void onSubmit()
+//			{
+//				info("selected person(s): " + palette.getDefaultModelObjectAsString());
+//				IModel modelAdherents  = palette.getDefaultModel();
+//				List<Adherent> adherents = (List<Adherent>) modelAdherents.getObject();
+//				for(Adherent adherent : adherents){
+//					getResaSession().getPlongeeService().fairePasserAttenteAInscrit(plongee, adherent);
+//				}
+//			}
+			
+		};
+
+		AjaxButton valid = new AjaxButton("valid") {
 			@Override
-			protected void onSubmit()
-			{
-				info("selected person(s): " + palette.getDefaultModelObjectAsString());
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				
 				IModel modelAdherents  = palette.getDefaultModel();
 				List<Adherent> adherents = (List<Adherent>) modelAdherents.getObject();
 				for(Adherent adherent : adherents){
 					getResaSession().getPlongeeService().fairePasserAttenteAInscrit(plongee, adherent);
 				}
+				setResponsePage(AccueilPage.class);
+			}
+			
+			// L'implémentation de cette méthode est nécessaire pour voir
+			// les messages d'erreur dans le feedBackPanel
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.addComponent(feedback);
 			}
 		};
-
+		
+		Link cancel = new Link("cancel") {
+			@Override
+			public void onClick() {
+				setResponsePage(AccueilPage.class);
+			}
+		};
+		
+		form.add(valid);
+		form.add(cancel);
 		add(form);
 		form.add(palette);
 
-		add(new FeedbackPanel("feedback"));
+//		add(new FeedbackPanel("feedback"));
 	}
 
 }
