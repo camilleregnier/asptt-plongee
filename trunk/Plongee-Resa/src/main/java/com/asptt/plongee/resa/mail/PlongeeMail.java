76b8441ec886129edbe9ffdb6720b23a24633867
eric.simon28@orange.fr <eric.simon28@orange.fr@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-20 20:12:53 +0000

    plongeMail sans commentaire

--- trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/mail/PlongeeMail.java
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/mail/PlongeeMail.java
@@ -16,7 +16,6 @@ public class PlongeeMail {
 
 	private static PlongeeMail instance = null;
 	private Email email = null;
-//	private List<String> destis = null;
 
 	public PlongeeMail() throws MessagingException {
 		super();
@@ -32,22 +31,6 @@ public class PlongeeMail {
 		this.email.setFrom("eric.simon28@orange.fr");
 	}
 	
-//	public PlongeeMail(MailException me) throws ResaException {
-//		email = new SimpleEmail();
-//		email.setDebug(true);
-//		email.setHostName("smtp.orange.fr");
-//		email.setSubject(me.getSubject());
-//		try {
-//			email.setMsg(me.getMsg());
-//		} catch (MessagingException e) {
-//			e.printStackTrace();
-//			throw new ResaException("probleme avec le sujet du mail");
-//		}
-//		this.destis = me.getFroms();
-//		email.setFrom("camille.regnier@orange.fr");
-//		email.addTo("camille.regnier@gmail.com");
-//	}
-	
 	public PlongeeMail getInstance() throws MessagingException {
 		if(null == instance){
 			instance = new PlongeeMail();
@@ -56,22 +39,15 @@ public class PlongeeMail {
 	}
 	
 	public void sendMail(List<String> destis) throws ResaException{
-//		try {
-//			email.setSubject("Attention : liste d'attente !");
-//			email.setMsg("des plongeurs sont en liste d'attente pour le plongée du : ");
-			try {
-				for (String desti : destis){
-					email.addTo(desti);
-					email.send();
-				}
-			} catch (MessagingException e) {
-				e.printStackTrace();
-				throw new ResaException("pb lors de l'envoi d'un mail");
+		try {
+			for (String desti : destis){
+				email.addTo(desti);
+				email.send();
 			}
-//		} catch (MessagingException e) {
-//			e.printStackTrace();
-//			throw new ResaException("Problème lors de l'envoie du mail");
-//		}
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			throw new ResaException("pb lors de l'envoi d'un mail");
+		}
 	}
 
 	public Email getEmail() {
@@ -82,12 +58,4 @@ public class PlongeeMail {
 		this.email = email;
 	}
 
-//	public List<String> getDestis() {
-//		return destis;
-//	}
-//
-//	public void setDestis(List<String> destis) {
-//		this.destis = destis;
-//	}
-	
 }
