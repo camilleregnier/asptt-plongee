fdb94438b2d390f37130dfbe9b83bee929529ced
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-20 06:02:23 +0000

    Gestion de la stratégie URL's

--- Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/PlongeeApplication.java
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/PlongeeApplication.java
@@ -10,12 +10,26 @@ import org.apache.wicket.authentication.pages.SignInPage;
 import org.apache.wicket.authorization.strategies.role.RoleAuthorizationStrategy;
 import org.apache.wicket.authorization.strategies.role.metadata.MetaDataRoleAuthorizationStrategy;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.target.coding.HybridUrlCodingStrategy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.asptt.plongee.resa.ui.web.wicket.page.AccueilPage;
 import com.asptt.plongee.resa.ui.web.wicket.page.LoginPage;
+import com.asptt.plongee.resa.ui.web.wicket.page.LogoutPage;
+import com.asptt.plongee.resa.ui.web.wicket.page.ModifPasswordPage;
+import com.asptt.plongee.resa.ui.web.wicket.page.admin.AnnulerPlongee;
 import com.asptt.plongee.resa.ui.web.wicket.page.admin.CreerAdherent;
+import com.asptt.plongee.resa.ui.web.wicket.page.admin.CreerPlongee;
+import com.asptt.plongee.resa.ui.web.wicket.page.admin.GererAdherents;
+import com.asptt.plongee.resa.ui.web.wicket.page.admin.GererListeAttenteOne;
+import com.asptt.plongee.resa.ui.web.wicket.page.admin.GererPlongeeAOuvrirOne;
+import com.asptt.plongee.resa.ui.web.wicket.page.consultation.ConsulterPlongees;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.DeInscriptionPlongeePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.InscriptionPlongeePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.secretariat.DesInscriptionPlongeePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.secretariat.InscriptionAdherentPlongeePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.secretariat.InscriptionExterieurPlongeePage;
 
 public class PlongeeApplication extends AuthenticatedWebApplication {
 
@@ -37,9 +51,26 @@ public class PlongeeApplication extends AuthenticatedWebApplication {
 //				new RoleAuthorizationStrategy(new RoleAuthorizationStrategy()));
 //			MetaDataRoleAuthorizationStrategy.authorize(CreerAdherent.class, "ADMIN");
 //			MetaDataRoleAuthorizationStrategy.authorize(AdminInternalPage.class, "ADMIN");
+		
 		// mounting login page so that it can be referred to in the security
 		// constraint
-		mountBookmarkablePage("/login", LoginPage.class);		
+		mountBookmarkablePage("/login", LoginPage.class);
+		mountBookmarkablePage("/logout", LogoutPage.class);
+		mountBookmarkablePage("/consulter", ConsulterPlongees.class);
+		mountBookmarkablePage("/inscrire", InscriptionPlongeePage.class);
+		mountBookmarkablePage("/desinscrire", DeInscriptionPlongeePage.class);
+		mountBookmarkablePage("/accueil", AccueilPage.class);
+		mountBookmarkablePage("/modifpassword", ModifPasswordPage.class);
+		mountBookmarkablePage("/creeradherent", CreerAdherent.class);
+		mountBookmarkablePage("/creerplongee", CreerPlongee.class);
+		mountBookmarkablePage("/annulerplongee", AnnulerPlongee.class);
+		mountBookmarkablePage("/gereradherents", GererAdherents.class);
+		mountBookmarkablePage("/gererplongee", GererPlongeeAOuvrirOne.class);
+		mountBookmarkablePage("/gererlisteattente", GererListeAttenteOne.class);
+		mountBookmarkablePage("/secretariat/inscriptionadherent", InscriptionAdherentPlongeePage.class);
+		mountBookmarkablePage("/secretariat/inscriptionexterieur", InscriptionExterieurPlongeePage.class);
+		mountBookmarkablePage("/secretariat/desinscription", DesInscriptionPlongeePage.class);
+		
 	}
 
 	public Session newSession(Request request, Response response) {
