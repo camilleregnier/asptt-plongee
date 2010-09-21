8d22d69e8426c16e8a9dcedd06132b16ee3ce550
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-21 16:03:44 +0000

    Dataview dans les pages GereListeAttenteOne et GererPlongeeAOuvrirOne

--- Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/GererPlongeeAOuvrirOne.java
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/GererPlongeeAOuvrirOne.java
@@ -1,36 +1,24 @@
 package com.asptt.plongee.resa.ui.web.wicket.page.admin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.datetime.markup.html.basic.DateLabel;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
 import org.apache.wicket.feedback.IFeedback;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Check;
-import org.apache.wicket.markup.html.form.CheckBox;
-import org.apache.wicket.markup.html.form.CheckBoxMultipleChoice;
-import org.apache.wicket.markup.html.form.CheckGroup;
-import org.apache.wicket.markup.html.form.CheckGroupSelector;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.Radio;
-import org.apache.wicket.markup.html.form.RadioGroup;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.util.convert.converters.DateConverter;
 
-import com.asptt.plongee.resa.model.Adherent;
 import com.asptt.plongee.resa.model.Plongee;
-import com.asptt.plongee.resa.service.PlongeeService;
+import com.asptt.plongee.resa.model.PlongeeDataProvider;
 import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
 
 public class GererPlongeeAOuvrirOne extends TemplatePage {
@@ -47,46 +35,63 @@ public class GererPlongeeAOuvrirOne extends TemplatePage {
 		private static final long serialVersionUID = -1555366090072306934L;
 		
 		private List<Plongee> data;
-		RadioGroup<Plongee> group = new RadioGroup<Plongee>("group", new Model<Plongee>());
 
 		@SuppressWarnings("serial")
 		public GererListeAttenteOneForm(String name, IFeedback feedback) {
 
 			super(name);
-					
-			add(group);
 
 			data = getResaSession().getPlongeeService().rechercherPlongeeAOuvrir(
 					getResaSession().getPlongeeService().rechercherPlongeeTout());
-
-			ListView<Plongee> list = new ListView<Plongee>("plongeeList", data){
-				public void populateItem(ListItem<Plongee> listItem) {           
-					listItem.add(new Radio<Plongee>("radio", listItem.getModel()));
-					
-					// Formatage de la date affichée
-					Calendar cal = Calendar.getInstance();
-					cal.setTime(listItem.getModel().getObject().getDate());
-					String dateAffichee = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.FRANCE) + " ";
-					dateAffichee = dateAffichee + cal.get(Calendar.DAY_OF_MONTH) + " ";
-					dateAffichee = dateAffichee + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.FRANCE) + " ";
-					dateAffichee = dateAffichee + cal.get(Calendar.YEAR);
-					
-					listItem.add(new Label("date", dateAffichee));                
-					listItem.add(new Label("type",new PropertyModel<String>(listItem.getDefaultModel(), "type")));
-				}
 			
-			}.setReuseItems(true);
-			group.add(list);
+			PlongeeDataProvider pDataProvider = new PlongeeDataProvider(data);
+
+			add(new DataView<Plongee>("simple", pDataProvider) {
+				
+				protected void populateItem(final Item<Plongee> item) {          
+						Plongee plongee = item.getModelObject();
+						String nomDP = "Aucun";
+						if (null != plongee.getDp()) {
+							nomDP = plongee.getDp().getNom();
+						}
+
+						item.add(new IndicatingAjaxLink("select") {
+							@Override
+							public void onClick(AjaxRequestTarget target) {
+								setResponsePage(new GererPlongeeAOuvrirTwo(item.getModelObject()));
+							}
+						});
+
+						// Mise en forme de la date
+						Calendar cal = Calendar.getInstance();
+						cal.setTime(plongee.getDate());
+						String dateAffichee = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.FRANCE) + " ";
+						dateAffichee = dateAffichee + cal.get(Calendar.DAY_OF_MONTH) + " ";
+						dateAffichee = dateAffichee + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.FRANCE) + " ";
+						dateAffichee = dateAffichee + cal.get(Calendar.YEAR);
+						
+						item.add(new Label("date", dateAffichee));
+						item.add(new Label("dp", nomDP));
+						item.add(new Label("type", plongee.getType()));
+						item.add(new Label("niveauMini", plongee.getNiveauMinimum().toString()));
+						
+						// Places restantes
+						item.add(new Label("placesRestantes", getResaSession().getPlongeeService().getNbPlaceRestante(plongee).toString()));
+
+						item.add(new AttributeModifier("class", true,
+								new AbstractReadOnlyModel<String>() {
+									@Override
+									public String getObject() {
+										return (item.getIndex() % 2 == 1) ? "even"
+												: "odd";
+									}
+								}));
+					}
+					
+				
 			
+			});	
 		}
-
-		public void onSubmit() {
-			IModel<Plongee>  list = group.getModel();
-			PageParameters param = new PageParameters();
-			param.put("plongeeAOuvrir", list.getObject());
-			setResponsePage(new GererPlongeeAOuvrirTwo(list.getObject()));
-		}
-
 	}
 
 }
