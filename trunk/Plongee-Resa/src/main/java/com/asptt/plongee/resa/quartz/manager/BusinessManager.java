3afa03ffccb4b1f2f57b193bbd6dc8d0b9206a6a
eric.simon28@orange.fr <eric.simon28@orange.fr@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-26 10:20:30 +0000

    le jeudi n'est plus créé en hiver

--- trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/quartz/manager/BusinessManager.java
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/quartz/manager/BusinessManager.java
@@ -38,65 +38,66 @@ public class BusinessManager {
 		GregorianCalendar gc = new GregorianCalendar();
 		gc.setTime(dateDuJour);
 		
-//		//------------------ +8 jours-------------------------------------------
-//		// par defaut les plongées sont à 20 plongeurs et n'ont pas de niveau mini
-//		plongee.setNbMaxPlaces(20);
-////		plongee.setNiveauMinimum(null);
-//		plongee.setOuvertureForcee(true);
-//		//Plongée du MERCREDI Aprem
-//		gc.add(GregorianCalendar.DATE, +3);
-//		plongee.setType(Plongee.Type.APRES_MIDI);
-//		plongee.setDate(gc.getTime());
-//		plongeeService.creerPlongee(plongee);
-//		System.out.println("Plongée du MERCREDI aprem créee : "+gc.getTime().toString());
-//	
-//		//Plongée du JEUDI Soir
-//		gc.setTime(dateDuJour);
-//		gc.add(GregorianCalendar.DATE, +4);
-//		plongee.setType(Plongee.Type.SOIR);
-//		plongee.setDate(gc.getTime());
-//		plongeeService.creerPlongee(plongee);
-//		System.out.println("Plongée du JEUDI soir créee : "+gc.getTime().toString());
-//			
-//		//Plongée du VENDREDI Aprem
-//		gc.setTime(dateDuJour);
-//		gc.add(GregorianCalendar.DATE, +5);
-//		plongee.setType(Plongee.Type.APRES_MIDI);
-//		plongee.setDate(gc.getTime());
-//		plongeeService.creerPlongee(plongee);
-//		System.out.println("Plongée du VENDREDI aprem créee : "+gc.getTime().toString());
-//
-//		//Plongée du SAMEDI matin
-//		gc.setTime(dateDuJour);
-//		gc.add(GregorianCalendar.DATE, +6);
-//
-//		plongee.setType(Plongee.Type.MATIN);
-//		plongee.setDate(gc.getTime());
-//		plongeeService.creerPlongee(plongee);
-//		System.out.println("Plongée du SAMEDI matin créee : "+gc.getTime().toString());
-//		//Plongée du SAMEDI aprem
-//		plongee.setType(Plongee.Type.APRES_MIDI);
-//		plongee.setDate(gc.getTime());
-//		plongeeService.creerPlongee(plongee);
-//		System.out.println("Plongée du SAMEDI aprem créee : "+gc.getTime().toString());
-//
-//		//Plongée du DIMANCHE matin
-//		gc.setTime(dateDuJour);
-//		gc.add(GregorianCalendar.DATE, +7);
-//		plongee.setType(Plongee.Type.MATIN);
-//		plongee.setDate(gc.getTime());
-//		plongeeService.creerPlongee(plongee);
-//		System.out.println("Plongée du DIMANCHE matin créee : "+gc.getTime().toString());
-
-		//------------------ +15 jours-------------------------------------------
-		// par defaut les plongées sont à 20 plongeurs et n'ont pas de niveau mini
-		plongee.setNbMaxPlaces(20);
-//		plongee.setNiveauMinimum(null);
-		plongee.setOuvertureForcee(true);
-		
-		
 		try {
+			//------------------ +8 jours-------------------------------------------
+			// par defaut les plongées sont à 20 plongeurs et n'ont pas de niveau mini
+//			plongee.setNbMaxPlaces(20);
+//			//plongee.setNiveauMinimum(null);
+//			plongee.setOuvertureForcee(true);
+//			//Plongée du MERCREDI Aprem
+//			gc.add(GregorianCalendar.DATE, +3);
+//			plongee.setType(Plongee.Type.APRES_MIDI);
+//			plongee.setDate(gc.getTime());
+//			plongeeService.creerPlongee(plongee);
+//			System.out.println("Plongée du MERCREDI aprem créee : "+gc.getTime().toString());
+//		
+//			//Plongée du JEUDI Soir
+////			gc.setTime(dateDuJour);
+////			gc.add(GregorianCalendar.DATE, +4);
+////			plongee.setType(Plongee.Type.SOIR);
+////			plongee.setDate(gc.getTime());
+////			plongeeService.creerPlongee(plongee);
+////			System.out.println("Plongée du JEUDI soir créee : "+gc.getTime().toString());
+//				
+//			//Plongée du VENDREDI Aprem
+//			gc.setTime(dateDuJour);
+//			gc.add(GregorianCalendar.DATE, +5);
+//			plongee.setType(Plongee.Type.APRES_MIDI);
+//			plongee.setDate(gc.getTime());
+//			plongeeService.creerPlongee(plongee);
+//			System.out.println("Plongée du VENDREDI aprem créee : "+gc.getTime().toString());
+//	
+//			//Plongée du SAMEDI matin
+//			gc.setTime(dateDuJour);
+//			gc.add(GregorianCalendar.DATE, +6);
+//	
+//			plongee.setType(Plongee.Type.MATIN);
+//			plongee.setDate(gc.getTime());
+//			plongeeService.creerPlongee(plongee);
+//			System.out.println("Plongée du SAMEDI matin créee : "+gc.getTime().toString());
+//			//Plongée du SAMEDI aprem
+//			plongee.setType(Plongee.Type.APRES_MIDI);
+//			plongee.setDate(gc.getTime());
+//			plongeeService.creerPlongee(plongee);
+//			System.out.println("Plongée du SAMEDI aprem créee : "+gc.getTime().toString());
+//	
+//			//Plongée du DIMANCHE matin
+//			gc.setTime(dateDuJour);
+//			gc.add(GregorianCalendar.DATE, +7);
+//			plongee.setType(Plongee.Type.MATIN);
+//			plongee.setDate(gc.getTime());
+//			plongeeService.creerPlongee(plongee);
+//			System.out.println("Plongée du DIMANCHE matin créee : "+gc.getTime().toString());
+	
+			//------------------ +15 jours-------------------------------------------
+			// par defaut les plongées sont à 20 plongeurs et n'ont pas de niveau mini
+			plongee.setNbMaxPlaces(20);
+			//plongee.setNiveauMinimum(null);
+			plongee.setOuvertureForcee(true);
+		
+		
 			//Plongée du MERCREDI Aprem
+			gc.setTime(dateDuJour);
 			gc.add(GregorianCalendar.DATE, +10);
 			plongee.setType(Plongee.Type.APRES_MIDI);
 			plongee.setDate(gc.getTime());
@@ -104,12 +105,12 @@ public class BusinessManager {
 			System.out.println("Plongée du MERCREDI aprem créee : "+gc.getTime().toString());
 
 			//Plongée du JEUDI Soir
-			gc.setTime(dateDuJour);
-			gc.add(GregorianCalendar.DATE, +11);
-			plongee.setType(Plongee.Type.SOIR);
-			plongee.setDate(gc.getTime());
-			plongeeService.creerPlongee(plongee);
-			System.out.println("Plongée du JEUDI soir créee : "+gc.getTime().toString());
+//			gc.setTime(dateDuJour);
+//			gc.add(GregorianCalendar.DATE, +11);
+//			plongee.setType(Plongee.Type.SOIR);
+//			plongee.setDate(gc.getTime());
+//			plongeeService.creerPlongee(plongee);
+//			System.out.println("Plongée du JEUDI soir créee : "+gc.getTime().toString());
 				
 			//Plongée du VENDREDI Aprem
 			gc.setTime(dateDuJour);
