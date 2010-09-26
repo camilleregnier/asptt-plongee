c56dbb5368e0d1ab8a4ef66913c79818fc78b11b
eric.simon28@orange.fr <eric.simon28@orange.fr@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-09-26 10:26:26 +0000

    Class provisoire : en attente des listes definitives

--- Plongee-Resa/src/main/java/com/asptt/plongee/resa/mail/PlongeeMail.java
+++ Plongee-Resa/src/main/java/com/asptt/plongee/resa/mail/PlongeeMail.java
@@ -1,6 +1,8 @@
 package com.asptt.plongee.resa.mail;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import javax.mail.MessagingException;
 
@@ -10,6 +12,7 @@ import org.apache.commons.mail.SimpleEmail;
 import com.asptt.plongee.resa.exception.MailException;
 import com.asptt.plongee.resa.exception.ResaException;
 import com.asptt.plongee.resa.model.Plongee;
+import com.asptt.plongee.resa.util.Parameters;
 
 public class PlongeeMail {
 
@@ -38,8 +41,14 @@ public class PlongeeMail {
 		return instance;
 	}
 	
-	public void sendMail(List<String> destis) throws ResaException{
+	public void sendMail(String destinataire) throws ResaException{
+		List<String> destis = null;
 		try {
+			if(destinataire.equalsIgnoreCase("ADMIN")){
+				destis = getDestisAdmin();
+			} else if (destinataire.equalsIgnoreCase("ENCADRANT")){
+				destis = getDestisEncadrant();
+			}
 			for (String desti : destis){
 				email.addTo(desti);
 				email.send();
@@ -58,4 +67,50 @@ public class PlongeeMail {
 		this.email = email;
 	}
 
+	public List<String> getDestisAdmin(){
+		//récupération du mails des admins
+        String admin = Parameters.getString("MAIL_DESTIS_ADMIN");
+        StringTokenizer st = new StringTokenizer(admin, ";");
+        List<String> destis = new ArrayList<String>();
+//        while (st.hasMoreElements()){
+//        	destis.add((String)st.nextElement());
+//        }
+//		destis.add("camille.regnier@orange.fr");
+		destis.add("eric.simon28@orange.fr");
+		return destis;
+	}
+
+	public List<String> getDestisEncadrant(){
+		//récupération du mails des admins
+        String admin = Parameters.getString("MAIL_DESTIS_ENCADRANT");
+        StringTokenizer st = new StringTokenizer(admin, ";");
+		List<String> destis = new ArrayList<String>();
+//	    while (st.hasMoreElements()){
+//	    	destis.add((String)st.nextElement());
+//	    }
+//		destis.add("camille.regnier@orange.fr");
+		destis.add("eric.simon28@orange.fr");
+//		destis.add("gilbert.dicostanzo@orange.fr");
+//		destis.add("michel-a.marchetti@orange.fr");
+//		destis.add("phanie.laurent@wanadoo.fr");//BAUDOIN
+//		//destis.add("boulet@boulet.com");//BOULET
+//		destis.add("viviane.valerian@orange.fr");//CANTO
+//		destis.add("daniel.cavenati@free.fr");//CAVENATI
+//		destis.add("cyana83@wanadoo.fr");//DANGLEANT
+//		destis.add("benoit.delagarde@free.fr");//DELAGARDE
+//		destis.add("philippe.fremy@orange.fr");//FREMY
+//		destis.add("jpgallice@hotmail.com");//GALLICE
+//		destis.add("germanique.jean-claude@neuf.fr");//GERMANIQUE
+//		destis.add("gomez348@free.fr");//GOMEZ
+//		destis.add("blue.bibi@free.fr");//HAMAIDE
+//		destis.add("jeanpierre.maures@dekra.com");//MAURES
+//		destis.add("mautalen1@aliceadsl.fr");//MAUTALEN
+//		destis.add("picber@club-internet.fr");//PICCOLO
+//		destis.add("gisele.marseille@wanadoo.fr");//PUGET
+//		destis.add("philippe.rojas@gmail.com");//ROJAS
+//		destis.add("jteffene@orange.fr");//TEFFENE
+//		destis.add("hamou.zerrifi@orange-ftgroup.com");//HAMOU
+		return destis;
+	}
+	
 }
