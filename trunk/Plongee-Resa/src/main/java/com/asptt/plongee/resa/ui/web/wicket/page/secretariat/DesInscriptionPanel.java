974655d4202be712f80bf6affda3f3b067446829
eric.simon28@orange.fr <eric.simon28@orange.fr@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2010-10-28 19:14:58 +0000

    nouveu package secretariat

---
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/secretariat/DesInscriptionPanel.java
@@ -0,0 +1,177 @@
+package com.asptt.plongee.resa.ui.web.wicket.page.secretariat;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.Page;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.behavior.IBehavior;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.CompoundPropertyModel;
+
+import com.asptt.plongee.resa.model.Adherent;
+import com.asptt.plongee.resa.model.Plongee;
+import com.asptt.plongee.resa.model.PlongeesAdherentDataProvider;
+import com.asptt.plongee.resa.ui.web.wicket.ResaSession;
+
+public abstract class DesInscriptionPanel extends Panel {
+
+	private static final long serialVersionUID = -1601864562144567667L;
+	final ModalWindow modalConfirm;
+
+	public DesInscriptionPanel(String id, final Adherent plongeur) {
+		super(id, new CompoundPropertyModel<Adherent>(plongeur));
+		setOutputMarkupId(true);
+
+		final ResaSession resaSession = (ResaSession) getApplication()
+				.getSessionStore().lookup(getRequest());
+
+		// Initialisation de la fenêtre modal de confirmation d'annulation
+		add(modalConfirm = new ModalWindow("modalConfirm"));
+		//modalConfirm.setPageMapName("modal-2");
+
+		modalConfirm.setTitle("Confirmation");
+
+		modalConfirm.setResizable(false);
+		modalConfirm.setInitialWidth(30);
+		modalConfirm.setInitialHeight(15);
+		modalConfirm.setWidthUnit("em");
+		modalConfirm.setHeightUnit("em");
+
+		modalConfirm.setCssClassName(ModalWindow.CSS_CLASS_BLUE);
+		
+
+		// La liste des plongées auxquelle le plongeur est inscrit
+		add(new DataView<Plongee>("simple", new PlongeesAdherentDataProvider(
+				plongeur, resaSession.getPlongeeService())) {
+
+			private static final long serialVersionUID = 4491729538132325557L;
+
+			protected void populateItem(final Item<Plongee> item) {
+				final Plongee plongee = item.getModelObject();
+				String nomDP = "Aucun";
+				if (null != plongee.getDp()) {
+					nomDP = plongee.getDp().getNom();
+				}
+
+				item.add(new IndicatingAjaxLink("select") {
+					@Override
+					public void onClick(final AjaxRequestTarget target) {						
+						
+						modalConfirm.setPageCreator(new ModalWindow.PageCreator()
+						{
+							public Page createPage()
+							{
+								return new ConfirmSelectionModal(modalConfirm, plongee, plongeur);
+							}
+						});
+						modalConfirm.show(target);
+					}
+
+				});
+
+				// Mise en forme de la date
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(plongee.getDate());
+				String dateAffichee = cal.getDisplayName(Calendar.DAY_OF_WEEK,
+						Calendar.LONG, Locale.FRANCE)
+						+ " ";
+				dateAffichee = dateAffichee + cal.get(Calendar.DAY_OF_MONTH)
+						+ " ";
+				dateAffichee = dateAffichee
+						+ cal.getDisplayName(Calendar.MONTH, Calendar.LONG,
+								Locale.FRANCE) + " ";
+				dateAffichee = dateAffichee + cal.get(Calendar.YEAR);
+
+				item.add(new Label("date", dateAffichee));
+				item.add(new Label("dp", nomDP));
+				item.add(new Label("type", plongee.getType()));
+				item.add(new Label("niveauMini", plongee.getNiveauMinimum()
+						.toString()));
+
+				// Places restantes
+				item.add(new Label("placesRestantes", resaSession
+						.getPlongeeService().getNbPlaceRestante(plongee)
+						.toString()));
+
+				item.add(new AttributeModifier("class", true,
+						new AbstractReadOnlyModel<String>() {
+							@Override
+							public String getObject() {
+								return (item.getIndex() % 2 == 1) ? "even"
+										: "odd";
+							}
+						}));
+			}
+		});
+	}
+
+	public abstract void onSave(AjaxRequestTarget target, Plongee plongee, Adherent plongeur);
+
+	public class ConfirmSelectionModal extends WebPage
+	{
+		public ConfirmSelectionModal(final ModalWindow window, final Plongee plongee,
+				final Adherent plongeur)
+		{
+			// Informations précisant la plongeur concerné et la plongée
+			// dans la fenêtre de confirmation de désinscription
+			String infoPlongeur = "Etes-vous sûr de vouloir annuler l'inscription de " + plongeur.getPrenom() + " " + plongeur.getNom();
+			String infoPlongee =  "à la plongée du " + plongee.getDate() + " " + plongee.getType() + " ?";
+			String infoEncadrant = "";
+			
+			if(plongeur.getEncadrement() != null){
+				ResaSession resaSession = (ResaSession) getApplication()
+				.getSessionStore().lookup(getRequest());
+				
+				//C'est un encadrant : on regarde s'il en reste assez
+				if( ! resaSession.getPlongeeService().isEnoughEncadrant(plongee)){
+					infoEncadrant = infoEncadrant + "Il ne reste plus assez d'encadrant !!";
+				}
+			}
+			add(new Label("infoPlongeur", infoPlongeur));
+			add(new Label("infoPlongee", infoPlongee));
+			add(new Label("infoEncadrant", infoEncadrant));
+			
+			// Le lien qui va fermer la fenêtre de confirmation
+			// et appeler la méthode de désinscription de la page principale (DesInscriptionPlongeePage)
+			add(new IndicatingAjaxLink("yes")
+			{
+			
+				private static final long serialVersionUID = -8801329059174173909L;	
+
+				@Override
+				public void onClick(AjaxRequestTarget target)
+				{
+					onSave(target, plongee, plongeur);
+					modalConfirm.close(target);
+					
+				}
+			});
+
+			add(new IndicatingAjaxLink("no")
+			{
+				private static final long serialVersionUID = 7770018649246290638L;
+
+				@Override
+				public void onClick(AjaxRequestTarget target)
+				{
+					modalConfirm.close(target);
+				}
+			});
+		}
+
+	}
+
+}
---
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/secretariat/InscriptionAdherentPlongeePage.java
@@ -0,0 +1,107 @@
+package com.asptt.plongee.resa.ui.web.wicket.page.secretariat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxSubmitButton;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.string.Strings;
+import org.wicketstuff.objectautocomplete.AutoCompletionChoicesProvider;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteBuilder;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteField;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteRenderer;
+
+import com.asptt.plongee.resa.model.Adherent;
+import com.asptt.plongee.resa.model.AdherentDataProvider;
+import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.InscriptionPlongeePage;
+
+public class InscriptionAdherentPlongeePage extends TemplatePage {
+	
+	private List<Adherent> list;
+
+	public InscriptionAdherentPlongeePage() {
+		super();
+		add(new AdherentForm("form"));
+
+	}
+
+	public List<Adherent> getMatchingAdherents(String search) {
+		if (Strings.isEmpty(search)) {
+			List<Adherent> emptyList = Collections.emptyList();
+			return emptyList;
+		}
+		
+		if (list == null) {
+			list = getResaSession().getAdherentService()
+				.rechercherAdherentsActifs();
+		}
+		
+		ArrayList<Adherent> newList = new ArrayList<Adherent>();
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getNom().startsWith(search.toUpperCase())) {
+				newList.add(list.get(i));
+			}
+		}
+		return newList;
+	}
+	
+	class AdherentForm extends Form{
+
+		private static final long serialVersionUID = 5529259540931669786L;
+		ObjectAutoCompleteField<Adherent, String> autocompleteField ;
+
+		public AdherentForm(String id) {
+			super(id);
+			
+			AutoCompletionChoicesProvider<Adherent> provider = new AutoCompletionChoicesProvider<Adherent>() {
+				private static final long serialVersionUID = 1L;
+
+				public Iterator<Adherent> getChoices(String input) {
+					return getMatchingAdherents(input).iterator();
+				}
+			};
+			
+			ObjectAutoCompleteRenderer<Adherent> renderer = new ObjectAutoCompleteRenderer<Adherent>(){
+				private static final long serialVersionUID = 1L;
+
+				protected String getIdValue(Adherent adherent) {
+					return adherent.getNumeroLicense();
+				}
+				protected String getTextValue(Adherent adherent) {
+					String texteAffiche = adherent.getNom() + " " + adherent.getPrenom() + " " + ((adherent.getEncadrement() != null) ? adherent
+							.getEncadrement() : adherent.getNiveau());
+					return texteAffiche;
+				}
+			};
+			
+			ObjectAutoCompleteBuilder<Adherent, String> builder = new ObjectAutoCompleteBuilder<Adherent, String>(provider);
+			builder.autoCompleteRenderer(renderer);
+			builder.searchLinkText("Autre recherche");
+			builder.width(200);
+
+
+			autocompleteField = builder.build("numeroLicense", new Model<String>());
+			final TextField<String> adherent = autocompleteField.getSearchTextField();
+			adherent.setRequired(true);
+			
+			
+			add(autocompleteField);
+			
+			add(new IndicatingAjaxSubmitButton("valider", this) {
+
+				@Override
+				protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+					setResponsePage(new InscriptionPlongeePage(getResaSession().getAdherentService().rechercherAdherentParIdentifiant(autocompleteField.getConvertedInput())));
+				}
+
+			});
+		}
+	}
+
+}
