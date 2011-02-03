f075044bdb76d1a05143e202ed82f5c7606d22a3
camille.regnier <camille.regnier@83092e06-a3cd-234e-76c7-1de7de0cbb35>
2011-02-03 21:17:50 +0000

    Champ de recherche dans la page de gestion des adhérents

--- trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/GererAdherents.java
+++ trunk/Plongee-Resa/src/main/java/com/asptt/plongee/resa/ui/web/wicket/page/admin/GererAdherents.java
@@ -1,26 +1,60 @@
 package com.asptt.plongee.resa.ui.web.wicket.page.admin;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxSubmitButton;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.string.Strings;
+import org.wicketstuff.objectautocomplete.AutoCompletionChoicesProvider;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteBuilder;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteField;
+import org.wicketstuff.objectautocomplete.ObjectAutoCompleteRenderer;
 
 import com.asptt.plongee.resa.model.Adherent;
 import com.asptt.plongee.resa.model.AdherentDataProvider;
+import com.asptt.plongee.resa.model.DetachableAdherentModel;
 import com.asptt.plongee.resa.ui.web.wicket.page.TemplatePage;
+import com.asptt.plongee.resa.ui.web.wicket.page.inscription.InscriptionPlongeePage;
 
 public class GererAdherents extends TemplatePage {
 
 	private ModalWindow modal2;
+	private List<Adherent> list;
+	
+	public List<Adherent> getMatchingAdherents(String search) {
+		if (Strings.isEmpty(search)) {
+			List<Adherent> emptyList = Collections.emptyList();
+			return emptyList;
+		}
+		
+		if (list == null) {
+			list = getResaSession().getAdherentService()
+				.rechercherAdherentsActifs();
+		}
+		
+		ArrayList<Adherent> newList = new ArrayList<Adherent>();
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getNom().startsWith(search.toUpperCase())) {
+				newList.add(list.get(i));
+			}
+		}
+		return newList;
+	}
 
 	@SuppressWarnings("serial")
 	public GererAdherents() {
@@ -77,6 +111,9 @@ public class GererAdherents extends TemplatePage {
 		};
 		add(dataView);
 		add(new PagingNavigator("navigator", dataView));
+		
+		add(new AdherentForm("form"));
+		
 	}
 	
 	private void replaceModalWindow(AjaxRequestTarget target, IModel<Adherent> adherent) {
@@ -87,4 +124,58 @@ public class GererAdherents extends TemplatePage {
 		// Pour éviter le message de disparition de la fenetre lors de la validation
 		target.appendJavascript( "Wicket.Window.unloadConfirmation  = false;");
 		}
+	
+	class AdherentForm extends Form{
+
+		private static final long serialVersionUID = 5529259540931669786L;
+		ObjectAutoCompleteField<Adherent, String> autocompleteField ;
+
+		public AdherentForm(String id) {
+			super(id);
+			
+			AutoCompletionChoicesProvider<Adherent> provider = new AutoCompletionChoicesProvider<Adherent>() {
+				private static final long serialVersionUID = 1L;
+
+				public Iterator<Adherent> getChoices(String input) {
+					return getMatchingAdherents(input).iterator();
+				}
+			};
+			
+			ObjectAutoCompleteRenderer<Adherent> renderer = new ObjectAutoCompleteRenderer<Adherent>(){
+				private static final long serialVersionUID = 1L;
+
+				protected String getIdValue(Adherent adherent) {
+					return adherent.getNumeroLicense();
+				}
+				protected String getTextValue(Adherent adherent) {
+					String texteAffiche = adherent.getNom() + " " + adherent.getPrenom() + " " + (adherent.getPrerogative());
+					return texteAffiche;
+				}
+			};
+			
+			ObjectAutoCompleteBuilder<Adherent, String> builder = new ObjectAutoCompleteBuilder<Adherent, String>(provider);
+			builder.autoCompleteRenderer(renderer);
+			builder.searchLinkText("Autre recherche");
+			builder.width(200);
+
+
+			autocompleteField = builder.build("numeroLicense", new Model<String>());
+			final TextField<String> adherent = autocompleteField.getSearchTextField();
+			adherent.setRequired(true);
+		
+			
+			add(autocompleteField);
+			
+			add(new IndicatingAjaxSubmitButton("valider", this) {
+
+				@Override
+				protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+					IModel<Adherent> adh = new DetachableAdherentModel(getResaSession().getAdherentService().rechercherAdherentParIdentifiant(autocompleteField.getConvertedInput()));
+					replaceModalWindow(target, adh);
+					modal2.show(target);
+				}
+
+			});
+		}
+	}
 }
